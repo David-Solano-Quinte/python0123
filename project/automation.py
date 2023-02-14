a4cc056ed48fa41054f7cfa20afdbb72aa46a180
David SQ <david.solano1@unmsm.edu.pe>
2023-02-13 22:31:37 -0500

    final

--- project/automation.py
+++ project/automation.py
@@ -4,16 +4,16 @@ import db
 import requests
 from lxml import html
 from csv import writer
+from csv import reader
 import matplotlib as plt
 
 message="""
     1)Insertar data:
     2)Actualizar data del dolar
     3)Mostrar grafico del dolar
+    4)Buscar un producto
     0)Salir
 """
-
-
 def insertData():
     #obtiene la ruta absoluta
     path_=os.getcwd()+'\project\dataTienda.csv'
@@ -34,9 +34,6 @@ def insertData():
         writer_obj = writer(data_t)
         writer_obj.writerow(b for b in new_data)
         data_t.close()
-
-
-
 def updateDolar():
     url = 'https://api.apis.net.pe/v1/tipo-cambio-sunat' #tipo cambio sunat
     response = requests.get(url)
@@ -48,7 +45,6 @@ def updateDolar():
         writer_obj = writer(hd)
         writer_obj.writerow(update.values())
         hd.close()
-
 def grafDolar():
     datos_graf = pd.read_csv("Historico_Dolar.csv",header=0,sep=",") 
     grafico= datos_graf[["Precio", "Fecha"]]
@@ -61,24 +57,37 @@ def grafDolar():
     plt.title('Historico del dolar')
     plt.grid()
     plt.show()
-
-
-
-
+def buscar_producto():
+    path_=os.getcwd()+'\project\dataTienda.csv'
+    df = pd. read_csv (path_,header=0, sep = ";")
+    plb_busc = input("Ingrese el producto a buscar: ")
+    condicion=(df["NAME"]==plb_busc.upper())
+    print(df[condicion])
+    """columna_productos=df["NAME"]
+    lista_productos = list(columna_productos.values)
+    ubi = []
+    a=int(0)
+    for producto in lista_productos:
+        if plb_busc.lower() == producto.lower():
+            ubi.append(a)
+        a+=1
+    print (ubi)"""
 while True:
-    print(message)
-    a=int(input('ingrese la tarea a realizar: '))
     try:
+        print(message)
+        a=int(input('ingrese la tarea a realizar: '))
         if a == 1:
             insertData()
         elif a==2:
             updateDolar()
         elif a==3:
             grafDolar()
+        elif a ==4:
+            buscar_producto()
         elif a==0:
             break
     except:
-        pass
+        print ("error")
 
 
 
